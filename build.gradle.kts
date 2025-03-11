459f00e09f37b4d9351193f10d783f6a222c3207
Amirreza Gholami <jv233114@gmail.com>
2025-03-11 19:18:17 +0330

    change some configs

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import io.ktor.plugin.features.javaVersion
 val exposed_version: String by project
 val h2_version: String by project
 val kotlin_version: String by project
+val ktor_version: String by project
 val logback_version: String by project
 
 plugins {
@@ -36,40 +37,38 @@ repositories {
 
 
 dependencies {
-    implementation("io.ktor:ktor-server-core")
-    implementation("io.ktor:ktor-server-content-negotiation")
-    implementation("io.ktor:ktor-serialization-kotlinx-json")
+    // Ktor Core
+    implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("io.ktor:ktor-server-auth:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+    implementation("io.ktor:ktor-server-config-yaml:$ktor_version")
+
+    // Logging
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    // Database (Exposed + H2/PostgreSQL)
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposed_version")
     implementation("com.h2database:h2:$h2_version")
-    implementation("io.ktor:ktor-server-netty")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("io.ktor:ktor-server-config-yaml")
-    testImplementation("io.ktor:ktor-server-test-host")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation("org.postgresql:postgresql:42.7.4")
 
-    // Auth
-    implementation("io.ktor:ktor-server-auth")
-
-    // KtorAdmin library
+    // KtorAdmin
     val ktorAdmin = "0.0.1-alpha8"
     implementation("io.github.amirroid:KtorAdmin:$ktorAdmin")
     ksp("io.github.amirroid:KtorAdmin:$ktorAdmin")
 
-    // Postgres
-    implementation("org.postgresql:postgresql:42.7.4")
-
-    // BCrypt
+    // Password Hashing (BCrypt)
     implementation("at.favre.lib:bcrypt:0.10.2")
+
+    // Testing
+    testImplementation("io.ktor:ktor-server-test-host:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
 
-//tasks.withType<Jar> {
-//    manifest {
-//        attributes["Main-Class"] = "ir.amirreza.ApplicationKt"
-//    }
-//    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
-//}
 ktor {
     fatJar {
         archiveFileName.set("ktor-app.jar")
@@ -78,7 +77,7 @@ ktor {
 
 tasks.withType<Jar> {
     manifest {
-        attributes["Main-Class"] = "ir.amirreza.ApplicationKt"
+        attributes["Main-Class"] = application.mainClass
     }
 }
 tasks {
